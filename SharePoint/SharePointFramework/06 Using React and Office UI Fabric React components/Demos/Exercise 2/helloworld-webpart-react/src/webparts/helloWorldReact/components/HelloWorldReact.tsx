6ae29371d6a163da7027cb4268443341f0609378
Rick Van Rousselt <rick.vanrousselt@outlook.com>
2018-11-14 10:29:47 +0100

    Update HelloWorldReact.tsx

--- SharePoint/SharePointFramework/06 Using React and Office UI Fabric React components/Demos/Exercise 2/helloworld-webpart-react/src/webparts/helloWorldReact/components/HelloWorldReact.tsx
+++ SharePoint/SharePointFramework/06 Using React and Office UI Fabric React components/Demos/Exercise 2/helloworld-webpart-react/src/webparts/helloWorldReact/components/HelloWorldReact.tsx
@@ -9,7 +9,7 @@ export default class HelloWorldReact extends React.Component<IHelloWorldReactPro
   constructor() {
     super();
     this.state = { 
-      showPanel: false
+      showPanel: false  
     }; 
   } 
 
